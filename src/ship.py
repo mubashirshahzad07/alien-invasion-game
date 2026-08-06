c2d1282c5503d4380facacce47ba7b074556eff7
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-08-06 07:47:10 +0500

    Add: ship movements and shooting features

--- src/ship.py
+++ src/ship.py
@@ -10,8 +10,28 @@ class Ship:
 
         self.image = pygame.image.load("images/fighter.bmp")
         self.rect = self.image.get_rect()
-
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Precisely store and increase the value, even when ship speed is float
+        # - self.rect.x will store every value as int, when ship speed is float
+        # the values stored would be incorrect.
+        # To overcome this error we introduce a new attribute that will keep
+        # the float changes and at last is used to update self.rect.x value.
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+        self.settings = game.settings
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        # Update Rect object using modified self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at the current location."""
