180a3b902cae871d1828e302c85f2e4e4ce5fad4
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-08-07 15:07:36 +0500

    add: alien fleet

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -21,12 +22,16 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             self.clock.tick(60)
@@ -63,6 +68,7 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
@@ -80,6 +86,44 @@ class AlienInvasion:
     def _update_bullets(self):
         self.bullets.update()
         self._remove_bullets()
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.hit_edges():
+                self._change_aliens_fleet_direction()
+                break
+
+    def _change_aliens_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _create_fleet(self):
+        alien = Alien(self)
+
+        alien_width = alien.rect.width
+        alien_height = alien.rect.height
+
+        current_x = alien_width
+        current_y = alien_height
+
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x, y):
+        new_alien = Alien(self)
+        new_alien.x = x
+        new_alien.rect.x = x
+        new_alien.rect.y = y
+        self.aliens.add(new_alien)
 
 
 if __name__ == "__main__":
--- src/settings.py
+++ src/settings.py
@@ -13,3 +13,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # 1 means towards +x axis, -1 means towards -x axis
+        self.fleet_direction = 1
